e3d133af7180fb411abcf3d7edef6b8b3bcbe912
RobertoSanchezFormos <roberto.sanchez@formos.com>
2024-09-17 18:23:42 -0500

    Initial commit

---
+++ P1_TSP/TSP.py
@@ -0,0 +1,38 @@
+from typing import List
+
+from P1_TSP.util import generar_ciudades_con_distancias, plotear_ruta
+
+
+class TSP:
+    def __init__(self, ciudades, distancias):
+        self.ciudades = ciudades
+        self.distancias = distancias
+
+    def encontrar_la_ruta_mas_corta(self):
+        pass
+        # implementación aquí
+
+    def plotear_resultado(self, ruta: List[str], mostrar_anotaciones: bool = True):
+        plotear_ruta(self.ciudades, ruta, mostrar_anotaciones)
+
+
+def study_case_1():
+    n_cities = 10
+    ciudades, distancias = generar_ciudades_con_distancias(n_cities)
+    tsp = TSP(ciudades, distancias)
+    ruta = ciudades.keys()
+    # ruta = tsp.encontrar_la_ruta_mas_corta()
+    tsp.plotear_resultado(ruta)
+
+def study_case_2():
+    n_cities = 100
+    ciudades, distancias = generar_ciudades_con_distancias(n_cities)
+    tsp = TSP(ciudades, distancias)
+    ruta = ciudades.keys()
+    # ruta = tsp.encontrar_la_ruta_mas_corta()
+    tsp.plotear_resultado(ruta, False)
+
+
+if __name__ == "__main__":
+    # Solve the TSP problem
+    study_case_1()
---
+++ README.md
@@ -0,0 +1,9 @@
+# WorkShop1-USFQ
+## Taller 1 de inteligencia artificial
+
+- **Nombre del grupo**: xxxxxx
+- **Integrantes del grupo**:
+  * Estudiante 1
+  * Estudiante 2
+
+- **Link**: [Planificación en ClickUp](https://app.clickup.com/xxxxxx)
