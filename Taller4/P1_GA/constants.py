bd39e7b96c89a62e5d80e9a5f61f865f68fef40b
Milene <milene.munozm@gmail.com>
2024-10-19 15:19:38 -0500

    FINAL P1 CON CORRECCION DE FUNCION DISTANCIA

---
+++ Taller4/P1_GA/constants.py
@@ -0,0 +1,38 @@
+from enum import Enum
+MY_SEED = 123
+
+all_possible_gens = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !"
+
+class AptitudeType(str, Enum):
+    DEFAULT = 'default'
+    BY_DISTANCE = 'by_distance'
+    BY_DISTANCE_LEVENSHTEIN = 'by_distance_levenshtein'
+    NEW = 'new'
+
+
+class ParentSelectionType(str, Enum):
+    DEFAULT = 'default'
+    MIN_DISTANCE = 'min_distance'
+    NEW = 'new'
+
+class MutationType(str, Enum):
+    DEFAULT = 'default'
+    NEW = 'new'
+
+class CrossoverType(str, Enum):
+    DEFAULT = 'default'
+    NEW = 'new'
+
+class SelectionType(str, Enum):
+    DEFAULT = 'default'
+    NEW = 'new'
+
+class BestIndividualSelectionType(str, Enum):
+    DEFAULT = 'default'
+    MIN_DISTANCE = 'min_distance'
+    NEW = 'new'
+
+class NewGenerationType(str, Enum):
+    DEFAULT = 'default'
+    MIN_DISTANCE = 'min_distance'
+    NEW = 'new'
